693a40cd8b0111cfb7bf48314642814e50318eac
sscstack <shashanktech2@gmail.com>
2024-09-10 09:09:53 +0530

    Created Product Entity

---
+++ src/main/java/com/shashank/product_service/entities/Product.java
@@ -0,0 +1,20 @@
+package com.shashank.product_service.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private double price;
+    @Column(nullable = false)
+    private int quantity;
+}
