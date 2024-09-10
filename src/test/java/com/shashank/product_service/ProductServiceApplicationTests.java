edb8f9b1f1936ee302ba47bf31005b7d3fc0aaf4
sscstack <shashanktech2@gmail.com>
2024-09-10 08:57:42 +0530

    Initialized Product Service

---
+++ src/test/java/com/shashank/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashank.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
